12ed8702eff842f12be373cf1ce60f6822b3248b
Richard Tattersall <lordtatty@googlemail.com>
2022-02-23 15:55:18 +0000

    write out to csv file

--- get-list-title-csv-input/golang/main.go
+++ get-list-title-csv-input/golang/main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/Talis-Aspire-Developer-Community/examples/get-list-title-csv-input/golang/listapi"
 	"golang.org/x/oauth2/clientcredentials"
@@ -17,8 +18,9 @@ import (
 func main() {
 	// The list ID and tenant shortcode values are retrieved from the
 	// parameters passed when running this tool
-	var filename, tenant string
-	flag.StringVar(&filename, "file", "", "input csv file")
+	var infilepath, outfilepath, tenant string
+	flag.StringVar(&infilepath, "infile", "", "input csv file")
+	flag.StringVar(&outfilepath, "outfile", "", "input csv file")
 	flag.StringVar(&tenant, "tenant", "", "the tenant shortcode")
 	flag.Parse()
 
@@ -34,18 +36,38 @@ func main() {
 	// and will automatically refresh the token if it expires
 	c := getClient(personaID, personaSecret, personaURL)
 
-	// Open the file ready for reading
-	file, err := os.Open(filename)
+	if err := ensureOutFilepath(outfilepath); err != nil {
+		log.Fatal(fmt.Errorf("could not ensure out filepath: %w", err))
+	}
+
+	// Open the 'out' file ready for writing
+	out, err := os.Create(outfilepath)
 	if err != nil {
 		log.Fatal(fmt.Errorf("error opening csv file: %w", err))
 	}
-	defer file.Close()
+	defer out.Close()
 
+	// Open the 'in' file ready for reading
+	in, err := os.Open(infilepath)
+	if err != nil {
+		log.Fatal(fmt.Errorf("error opening csv file: %w", err))
+	}
+	defer in.Close()
+
+	// Iterate over the csv rows until we reach the end of file or error
+	if err := iterateAndWrite(in, out, tenant, c); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func iterateAndWrite(in io.Reader, out io.Writer, tenant string, c *http.Client) error {
 	// Create a new CSV reader
-	r := csv.NewReader(file)
+	r := csv.NewReader(in)
 	r.FieldsPerRecord = 1
 	r.TrimLeadingSpace = true
-	// Iterate over the csv rows until we reach the end of file or error
+	// Create a new CSV writer
+	w := csv.NewWriter(out)
+
 	for {
 		// Read each record from csv
 		record, err := r.Read()
@@ -53,20 +75,34 @@ func main() {
 			break
 		}
 		if err != nil {
-			log.Fatal(err)
+			return fmt.Errorf("couldnt read csv row from input: %w", err)
 		}
 		listID := record[0]
 
 		// Get the list from the URL
 		listResp, err := listapi.Get(tenant, listID, c)
 		if err != nil {
-			log.Fatal(fmt.Errorf("faild to get the list data: %w", err))
+			return fmt.Errorf("failed to get the list data: %w", err)
 		}
 
-		// Now we have the data in a struct, we can print out the title
+		// Now we have the data in a struct, we can write the title
+		// to the output file
 		title := listResp.Data.Attr.Title
-		fmt.Println(title)
+		outRow := []string{title}
+		w.Write(outRow)
+		w.Flush()
 	}
+	return nil
+}
+
+func ensureOutFilepath(f string) error {
+	absPath, err := filepath.Abs(f)
+	if err != nil {
+		return err
+	}
+	d := filepath.Dir(absPath)
+	fmt.Println(d)
+	return os.MkdirAll(d, os.ModePerm)
 }
 
 func getClient(ID, secret, url string) *http.Client {
